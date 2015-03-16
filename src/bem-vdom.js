af1534c49c5d95bfe46d21dc87a05f8e026b1549
Artem Shitov <artemshitov@yandex-team.ru>
2015-03-16 05:06:26 +0300

    Rewrite as mutable and drop dependencies on Immutable.js and Ramda

--- src/bem-vdom.js
+++ src/bem-vdom.js
@@ -1,14 +1,36 @@
-import R from 'ramda';
 import H from 'virtual-dom/h';
-import {Set, Map, List} from 'immutable';
+
+const modDelimiter = '_';
+const elDelimiter = '__';
+
+const bemClasses = function bemClasses (baseName, mods) {
+    let modClasses = Object.keys(mods)
+        .filter(function (key) {
+            return (typeof mods[key] === "boolean" && mods[key] === true) ||
+                   (typeof mods[key] === 'string' && mods[key].length !== 0);
+        })
+        .map(function (key) {
+            if (mods[key] === true) {
+                return baseName + modDelimiter + key;
+            } else {
+                return [baseName, key, mods[key]].join(modDelimiter);
+            }
+        });
+    modClasses.unshift(baseName);
+    return modClasses;
+};
+
+const flatten = function flatten (xs) {
+    return Array.prototype.concat.apply([], xs);
+};
 
 export class BEM {
     constructor (spec) {
         this.spec = spec;
     }
 
-    _modify (prop, value) {
-        return new this.constructor(R.assoc(prop, value, this.spec));
+    name () {
+        return this.spec.name;
     }
 
     tag () {
@@ -28,57 +50,62 @@ export class BEM {
     }
 
     setTag (newTag) {
-        return this._modify('tag', newTag);
+        this.spec.tag = newTag;
+        return this;
     }
 
     addClass (klass) {
-        return this._modify('classes', this.spec.classes.add(klass));
+        this.spec.classes.push(klass);
+        return this;
     }
 
     removeClass (klass) {
-        return this._modify('classes', this.spec.classes.remove(klass));
+        let i = this.spec.classes.indexOf(klass);
+        if (i !== -1) {
+            this.spec.classes.splice(i, 1);
+        }
+        return this;
     }
 
     setAttr (n, v) {
-        return this._modify('attrs', this.spec.attrs.set(n, v));
+        this.spec.attrs[n] = v;
+        return this;
     }
 
     removeAttr (attr) {
-        return this._modify('attrs', this.spec.attrs.remove(attr));
+        delete this.spec.attrs[attr];
+        return this;
     }
 
     setMod (mod, value) {
-        return this._modify('mods', this.spec.mods.set(mod, value));
+        this.spec.mods[mod] = value;
+        return this;
     }
 
     removeMod (mod) {
-        return this._modify('mods', this.spec.mods.remove(mod));
+        delete this.spec.mods[mod];
+        return this;
     }
 
     setName (name) {
-        return this._modify('name', name);
+        this.spec.name = name;
+        return this;
     }
 
     className (baseName) {
-        return this.mods()
-            .filter((v, k) => (R.type(v) === 'String' && v !== '') || v === true)
-            .map((v, k) => v === true ? `${baseName}_${k}` : `${baseName}_${k}_${v}`)
-            .toSet()
-            .add(baseName)
-            .union(this.spec.classes)
-            .toArray()
-            .join(' ');
-    };
+        return this.spec.classes.concat(bemClasses(baseName, this.spec.mods)).join(' ');
+    }
 
     vdom (parentName) {
         let className = this.className(this.baseName(parentName));
         let children = this.children().map(c =>
-            R.is(BEM, c) ? c.vdom(parentName || this.spec.name) : c
-        ).toArray();
-        let attrs = this.attrs().set('className', className).toJS();
+            c instanceof BEM ? c.vdom(parentName || this.name()) : c
+        );
+        let attrs = {className};
+        Object.keys(this.attrs()).forEach((key) => attrs[key] = this.attrs()[key]);
         
         return H(this.tag(), attrs, children);
-    };
+    }
 
     children () {
         return this.spec.children;
@@ -93,28 +120,29 @@ export class Block extends BEM {
     baseName () {
         return this.spec.name;
     }
-};
+}
 
 export class Element extends BEM {
     setBlockName (blockName) {
-        return this._modify('blockName', blockName);
+        this.spec.blockName = blockName;
+        return this;
     }
 
     baseName (parentName) {
         return this.spec.blockName !== void 0 ?
-            this.spec.blockName + '__' + this.spec.name :
-            parentName + '__' + this.spec.name;
+            this.spec.blockName + elDelimiter + this.spec.name :
+            parentName + elDelimiter + this.spec.name;
     }
-};
+}
 
 export const b = function (name, opts, ...children) {
     let {mods, attrs, tag, classes} = parseOpts(opts);
-    return new Block({name, mods, attrs, classes, tag, children: new List(R.flatten(children))});
+    return new Block({name, mods, attrs, classes, tag, children: flatten(children)});
 };
 
 export const qe = function (blockName, name, opts, ...children) {
     let {mods, attrs, tag, classes} = parseOpts(opts);
-    return new Element({name, mods, attrs, classes, tag, children: new List(R.flatten(children))});
+    return new Element({name, mods, attrs, classes, tag, children: flatten(children)});
 };
 
 export const e = function (name, opts, ...children) {
@@ -122,21 +150,25 @@ export const e = function (name, opts, ...children) {
 };
 
 function parseOpts (opts) {
-    let startsWith = pattern => str =>
-        R.substringTo(pattern.length, str) === pattern;
-    let _filter = (v, k) => startsWith('_')(k);
-    let mods = new Map(R.pickBy(R.not(_filter), opts));
-    let attrs = new Map(R.pipe(
-            R.pickBy(_filter),
-            R.omit(['_tag', '_className']),
-            R.toPairs,
-            R.map(([k, v]) => [R.substringFrom(1, k), v]),
-            R.fromPairs)(opts));
-    let tag = R.defaultTo('div', opts._tag);
-    let classes = R.contains(opts._className, ['', void 0]) ?
-        new Set() :
-        new Set(opts._className.split(' '));
+    let mods = {};
+    let attrs = {};
+    let tag = 'div';
+    let classes = [];
     
+    Object.keys(opts).forEach(function (key) {
+        if (key[0] === '_') {
+            if (key === '_tag') {
+                tag = opts[key];
+            } else if (key === '_className' && opts[key].length > 0) {
+                classes = opts[key].split(' ');
+            } else {
+                attrs[key.substr(1)] = opts[key];
+            }
+        } else {
+            mods[key] = opts[key];
+        }
+    });
+
     return {
         mods, tag, classes, attrs
     };
